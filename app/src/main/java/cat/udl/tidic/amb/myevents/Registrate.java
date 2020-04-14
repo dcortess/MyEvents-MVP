6c4437109120ba41c1eabd7b0106513e8304dd3b
Jordi Mateo <jordi.mateo@udl.cat>
2020-04-14 10:17:30 +0200

    Fix bug: Calendari amb la data seleccionada.

--- app/src/main/java/cat/udl/tidic/amb/myevents/Registrate.java
+++ app/src/main/java/cat/udl/tidic/amb/myevents/Registrate.java
@@ -7,14 +7,18 @@ import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 public class Registrate extends AppCompatActivity {
 
     private int contador=0;
+
 
 
     @Override
@@ -28,8 +32,7 @@ public class Registrate extends AppCompatActivity {
         final TextView name = (TextView) findViewById(R.id.User_name);
         final TextView description = (TextView) findViewById(R.id.User_Description);
         final CalendarView calendar = (CalendarView) findViewById(R.id.calendarView);
-        final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        final String selectedDate = sdf.format(new Date(calendar.getDate()));
+
 
         name.setText("");
         description.setText("");
@@ -37,6 +40,18 @@ public class Registrate extends AppCompatActivity {
         Bundle extras = getIntent().getExtras();
         assert extras != null;
         contador = extras.getInt("Contador");
+
+
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                Calendar c = Calendar.getInstance();
+                c.set(Calendar.YEAR, year);
+                c.set(Calendar.MONTH, month);
+                c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                calendar.setDate(c.getTimeInMillis());
+            }
+        });
 
         cancel.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,11 +67,13 @@ public class Registrate extends AppCompatActivity {
             public void onClick(View v) {
                 String get_name = name.getText().toString();
                 String get_description = description.getText().toString();
-                String get_date = selectedDate;
+                SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+                String selectedDate = sdf.format(new Date(calendar.getDate()));
+
                 Intent intent = new Intent(Registrate.this, Events.class);
                 intent.putExtra("name", get_name);
                 intent.putExtra("description", get_description);
-                intent.putExtra("date", get_date);
+                intent.putExtra("date", selectedDate);
                 intent.putExtra("Contador",contador);
                 startActivity(intent);
             }
